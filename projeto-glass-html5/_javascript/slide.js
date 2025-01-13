6931d761a94a40b28620905dd267e6862b8b2370
Alexandre Zanini Filho <alexandrezaninifilho@gmail.com>
2025-01-13 20:40:41 -0300

    Recriando
    Fazendo modificações no site para que ele fique mais parecido com sites modernos

---
+++ projeto-glass-html5/_javascript/slide.js
@@ -0,0 +1,18 @@
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(()=> {
+    proximaimg()
+}, 4000)
+
+function proximaimg(){
+    cont++
+
+    if(cont >= 4){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
